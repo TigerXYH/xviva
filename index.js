5cbfb2e5974159c8674c0ebd88e6449e2da9b19b
xlinux <ftha@qq.com>
2024-03-12 12:26:00 +0800

    ok

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function random(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-module.exports = random;
+// module.exports = random;
+
+// index.js
+
+const _ = require('lodash');
+
+const array = [1, 2, 3, 4, 5];
+console.log(_.sum(array));
